245df86e3b4dddf9e8662aa4fab9811bec3328df
administroot <1474668090@qq.com>
2025-02-13 15:22:12 +0800

    :sparkles: Introduce get_portion()

--- src/interface_page.rs
+++ src/interface_page.rs
@@ -32,10 +32,14 @@ impl FreeEta {
 
     /// Get Length::FillPortion(`value`)
     fn get_portion(&self, position: SpacePosition) -> u16 {
-        let blocks = 102;
+        let blocks = 102.;
+        let window_height = self.window_size.height;
+        let center_y = window_height / 2.;
+        let block_to_point = window_height / blocks;
+        let gap = (self.mouse_point.y - center_y) / block_to_point;
         match position {
-            SpacePosition::Upper => blocks / 2,
-            SpacePosition::Lower => blocks / 2,
+            SpacePosition::Upper => (blocks / 2. + gap) as u16,
+            SpacePosition::Lower => (blocks / 2. - gap) as u16,
         }
     }
 }
